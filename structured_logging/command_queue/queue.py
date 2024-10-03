0409ae35dc592e7bb1597024cd32516b5ee88f1a
Haraldur Steinar Skúlason <hallist@hey.com>
2024-10-03 18:59:32 +0000

    made a basic queue

--- structured_logging/command_queue/queue.py
+++ structured_logging/command_queue/queue.py
@@ -1,17 +1,25 @@
 import threading
+import time
+
 from structured_logging.command_queue.command import Command
 
 
 class Queue:
     # TODO: we also need to inject the async delay time into the constructor
-    def __init__(self):
-        raise NotImplementedError()
+    def __init__(self, async_delay: int) -> None:
+        self.__async_delay = async_delay
+        self.__queue = []
         self.__thread = threading.Thread(target=self.__process)
         self.__thread.daemon = True
         self.__thread.start()
 
     def add(self, command: Command):
-        raise NotImplementedError()
+        self.__queue.append(command)
 
     def __process(self):
-        raise NotImplementedError()
+        while True:
+            try:
+                command = self.__queue.pop(0)
+                command.execute()
+            except self.__queue == []:
+                time.sleep(self.__async_delay)
