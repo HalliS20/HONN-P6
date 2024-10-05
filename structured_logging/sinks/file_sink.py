32b6d15481beef835f5d4ba8c8265f7081365ca2
HansHT <johannes.htomas@gmail.com>
2024-10-05 14:08:13 +0000

    Capitalized FileSink class name

--- structured_logging/sinks/file_sink.py
+++ structured_logging/sinks/file_sink.py
@@ -1,7 +1,7 @@
 from structured_logging.sinks.i_sink import ISink
 import json
 
-class fileSink(ISink):
+class FileSink(ISink):
     def __init__(self, file_path: str) -> None:
         self.path = file_path
 
@@ -15,4 +15,4 @@ class fileSink(ISink):
 
 
 if __name__ == '__main__':
-    fileSink('test.json').sink_data({'test': 'test'})
+    FileSink('test.json').sink_data({'test': 'test'})
