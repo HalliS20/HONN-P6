984c2707bdf7b97a39cdd81b2531ddae02d142e1
Haraldur Steinar Skúlason <hallist@hey.com>
2024-10-05 16:37:26 +0000

    made client logger and config factory

--- client/infrastructure/logging/logger_config_factory.py
+++ client/infrastructure/logging/logger_config_factory.py
@@ -1,7 +1,16 @@
-from client.infrastructure.settings.settings import Settings
+from client.infrastructure.settings.settings import Settings, LoggingType
 from structured_logging.configuration.logger_config import LoggerConfig
 from structured_logging.logger_creation.logger_config_builder import LoggerConfigBuilder
 
 
 def create_logger_config(settings: Settings, builder: LoggerConfigBuilder) -> LoggerConfig:
-    raise NotImplementedError()
+    if settings.logging_type == LoggingType.CONSOLE:
+        builder.with_console_sink()
+    else :
+        builder.with_file_sink(settings.logging_file_path)
+    if settings.logging_is_async:
+        builder.as_async(settings.logging_async_delay)
+    builder.add_environment(settings.environment)
+    return builder.build()
+
+
